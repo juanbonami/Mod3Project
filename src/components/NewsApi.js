42fca2ac7f6a4125f79601175ff9ac83f4c61ef2
Juan Bonami <Juanbonami@gmail.com>
2020-05-24 01:06:53 -0500

    create class in newsApi

---
+++ src/components/NewsApi.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class NewsApi extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
