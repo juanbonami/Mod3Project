9f31693a8a938fb29f22b3a1420e2ba0ba76d378
Juan Bonami <Juanbonami@gmail.com>
2020-05-24 01:11:25 -0500

    added constructor and state

--- src/components/NewsApi.js
+++ src/components/NewsApi.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react'
 
 export default class NewsApi extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            data: []
+        }
+
+    }
+
+
+
+
     render() {
         return (
             <div>
