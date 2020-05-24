d1753231ae91173b5904298af29d00fe49a0e5aa
Juan Bonami <Juanbonami@gmail.com>
2020-05-24 01:44:16 -0500

    added component to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import NewsApi from './components/NewsApi';
 
 function App() {
   return (
     <div className="App">
-      
+      <NewsApi />
     </div>
   );
 }
