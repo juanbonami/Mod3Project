e1807c16ed6815b1e1d25ca53328c23e99f11b59
Juan Bonami <Juanbonami@gmail.com>
2020-05-26 04:43:45 -0500

    got response from backend localhost8080

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import NewsApi from './components/NewsApi';
+import BackendApi from './components/BackendApi';
 
 function App() {
   return (
     <div className="App">
       <NewsApi />
+      <BackendApi />
     </div>
   );
 }
--- src/components/BackendApi.js
+++ src/components/BackendApi.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 
 export default class BackendApi extends Component {
     constructor(props) {
@@ -9,11 +10,28 @@ export default class BackendApi extends Component {
         }
     }
 
+    componentDidMount() {
+
+        const api = 'http://localhost:8080/comments';
+
+        axios.get(api)
+        .then(Response => {
+            console.log(Response);
+            // this.setState({
+            //     info: Response
+            // })
+        })
+        .catch(error => {
+            console.log(error);
+        })
+
+    }
+
 
     render() {
         return (
             <div>
-                
+                {/* <h1> {this.state.info} </h1> */}
             </div>
         )
     }
