1abdac52e8b58a0e910c3a741d49d431efacdb17
Juan Bonami <Juanbonami@gmail.com>
2020-05-26 04:16:17 -0500

    set up constructor and state

--- src/components/BackendApi.js
+++ src/components/BackendApi.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react'
 
 export default class BackendApi extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            info: ''
+        }
+    }
+
+
     render() {
         return (
             <div>
